0a6f477c625660fc02ad7818bd6464c310f7b5e6
joan <joan92311@gmail.com>
2025-08-17 10:18:27 +0300

    inheritance

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
-from user import User
+from user import User  # Import the parent class
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # inherit first_name and last_name
+        self.knowledge = []  # empty list initially
+
+    def learn(self, info):
+        # add new knowledge to student's list
+        self.knowledge.append(info)
+
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-from user import User
-
 import random
+from user import User  # Import the parent class
+
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # inherit first_name and last_name
+        self.knowledge = knowledge  # store the knowledge list
 
     def teach(self):
-        pass
+        # return a random item from knowledge
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
